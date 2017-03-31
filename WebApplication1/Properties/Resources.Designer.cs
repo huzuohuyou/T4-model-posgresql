8ee7c6267a567cb300093808f4c6d85cd2c3cba4
wuhailong <13126506430@163.com>
2017-03-31 15:09:13 +0800

    1.update sln name

--- WebApplication1/Properties/Resources.Designer.cs
+++ WebApplication1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WebApplication1.Properties {
+namespace Com.HengBao.Pos.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WebApplication1.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WebApplication1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Com.HengBao.Pos.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
