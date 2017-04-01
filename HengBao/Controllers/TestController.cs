56f8c53ca4f2b7c61f22f7960d5dd546c1cdf063
wuhailong <13126506430@163.com>
2017-04-01 18:25:14 +0800

    1.add exceptionless modules

--- HengBao/Controllers/TestController.cs
+++ HengBao/Controllers/TestController.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Dapper;
 using xiaoheng.pos.Model;
-
+using Exceptionless;
 namespace HengBao.Controllers
 {
     public class TestController : ApiController
@@ -16,7 +16,7 @@ namespace HengBao.Controllers
         private const string CONNECTION_STRING = "server=127.0.0.1;Port=5432;Database=postgres;uid=postgres;pwd=wozuishuai;Encoding=UNICODE";
         [Route("Hello")]
         //怎们玩
-        public void Hello(string pname)
+        public bool Hello(string pname)
         {
             try
             {
@@ -25,11 +25,14 @@ namespace HengBao.Controllers
                 p.Name = "111";
                 IDbConnection conn = new NpgsqlConnection(CONNECTION_STRING);
                 var result = conn.Execute("Insert into product values (@name, @price, @status)", new { name = pname, price = 998, status = false });
-
+                //HttpConfiguration config = new HttpConfiguration();
+                //ExceptionlessClient.Default.RegisterWebApi(config);
+                //ExceptionlessClient.Default.RegisterWebApi(GlobalConfiguration.Configuration);
+                ExceptionlessClient.Default.CreateLog("Hello","Hello El!",LogLevel.Normal.ToString()).AddTags("exceptionless").Submit();
+                return true;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
                 throw;
             }
            }
